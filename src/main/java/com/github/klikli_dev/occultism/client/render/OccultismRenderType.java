c518b1d19ecec57d60cb19d4812e81e05e2f4002
klikli-dev <klikli@outlook.at>
2020-03-01 21:28:42 +0100

    added block selection rendering

---
+++ src/main/java/com/github/klikli_dev/occultism/client/render/OccultismRenderType.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright 2020 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.klikli_dev.occultism.client.render;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+
+import java.util.OptionalDouble;
+//region Initialization
+//endregion Initialization
+//endregion Initialization
+
+public class OccultismRenderType extends RenderType {
+    //region Fields
+    private static final LineState BLOCK_SELECTION_LINE_STATE = new LineState(OptionalDouble.of(2.0D));
+    public static final RenderType BLOCK_SELECTION = makeType("overlay_lines", DefaultVertexFormats.POSITION_COLOR, 1,
+            256, State.getBuilder().line(BLOCK_SELECTION_LINE_STATE).layer(PROJECTION_LAYERING)
+                         .transparency(TRANSLUCENT_TRANSPARENCY).texture(NO_TEXTURE).depthTest(DEPTH_ALWAYS)
+                         .cull(CULL_DISABLED).lightmap(LIGHTMAP_DISABLED).writeMask(COLOR_WRITE).build(false));
+    //endregion Fields
+
+    //region Initialization
+    public OccultismRenderType(String name, VertexFormat vertexFormat, int drawMode, int bufferSize,
+                               boolean useDelegate, boolean needsSorting, Runnable setupTaskIn,
+                               Runnable clearTaskIn) {
+        super(name, vertexFormat, drawMode, bufferSize, useDelegate, needsSorting, setupTaskIn, clearTaskIn);
+    }
+}
---
+++ src/main/java/com/github/klikli_dev/occultism/client/render/SelectedBlockRenderer.java
@@ -0,0 +1,120 @@
+/*
+ * MIT License
+ *
+ * Copyright 2020 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.klikli_dev.occultism.client.render;
+
+import com.github.klikli_dev.occultism.util.RenderUtil;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Matrix4f;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class SelectedBlockRenderer {
+
+    //region Fields
+    protected List<SelectionInfo> selectedBlocks = new ArrayList<>();
+    //endregion Fields
+
+    //region Methods
+
+    /**
+     * Highlights the given block position until the given system time (not game time)
+     *
+     * @param pos        the position to highlight.
+     * @param expireTime the time when it expires. Absolute system time, not interval!
+     */
+    public void selectBlock(BlockPos pos, long expireTime) {
+        this.selectedBlocks.add(new SelectionInfo(pos, expireTime));
+    }
+
+    /**
+     * Unselects the given block position.
+     *
+     * @param pos the position.
+     */
+    public void unselectBlock(BlockPos pos) {
+        this.selectedBlocks.removeIf(info -> info.selectedBlock.equals(pos));
+    }
+
+    @SubscribeEvent
+    public void renderWorldLastEvent(RenderWorldLastEvent event) {
+        this.renderSelectedBlocks(event);
+    }
+
+    protected void renderSelectedBlocks(RenderWorldLastEvent event) {
+        if (!this.selectedBlocks.isEmpty()) {
+            long time = System.currentTimeMillis();
+
+            MatrixStack matrixStack = event.getMatrixStack();
+            IRenderTypeBuffer.Impl buffer = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
+            IVertexBuilder builder = buffer.getBuffer(OccultismRenderType.BLOCK_SELECTION);
+            matrixStack.push();
+            Vec3d projectedView = Minecraft.getInstance().gameRenderer.getActiveRenderInfo().getProjectedView();
+            matrixStack.translate(-projectedView.x, -projectedView.y, -projectedView.z);
+            Matrix4f transform = matrixStack.getLast().getMatrix();
+
+            for (Iterator<SelectionInfo> it = this.selectedBlocks.iterator(); it.hasNext(); ) {
+                SelectionInfo info = it.next();
+
+                if (time > info.selectionExpireTime || info.selectedBlock == null) {
+                    //remove expired or invalid selections
+                    it.remove();
+                    return;
+                }
+                else {
+                    RenderUtil
+                            .buildBlockOutline(builder, transform, info.selectedBlock.getX(), info.selectedBlock.getY(),
+                                    info.selectedBlock.getZ(), 1.0f, 1.0f, 1.0f, 0.8f);
+                }
+            }
+
+            matrixStack.pop();
+            RenderSystem.enableTexture();
+            buffer.finish(OccultismRenderType.BLOCK_SELECTION);
+        }
+    }
+    //endregion Methods
+
+    public class SelectionInfo {
+        //region Fields
+        public BlockPos selectedBlock;
+        public long selectionExpireTime;
+        //endregion Fields
+
+        //region Initialization
+        public SelectionInfo(BlockPos selectedBlock, long selectionExpireTime) {
+            this.selectedBlock = selectedBlock;
+            this.selectionExpireTime = selectionExpireTime;
+        }
+        //endregion Initialization
+    }
+}
