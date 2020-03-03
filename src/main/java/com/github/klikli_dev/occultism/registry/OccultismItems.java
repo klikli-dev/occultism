e0c780a6d1c0ed4126b22232793132c7da964ede
klikli-dev <klikli@outlook.at>
2020-03-03 14:29:33 +0100

    added dimensional matrix item

--- src/main/java/com/github/klikli_dev/occultism/registry/OccultismItems.java
+++ src/main/java/com/github/klikli_dev/occultism/registry/OccultismItems.java
@@ -46,6 +46,11 @@ public class OccultismItems {
     public static final RegistryObject<Item> DEBUG_WAND = ITEMS.register("debug_wand",
             () -> new DebugWandItem(defaultProperties().maxStackSize(1)));
 
+    //Resources
+    //Components
+    public static final RegistryObject<Item> DIMENSIONAL_MATRIX = ITEMS.register("dimensional_matrix",
+            () -> new Item(new Item.Properties()));
+
     //Tools
     public static final RegistryObject<Item> STORAGE_REMOTE = ITEMS.register("storage_remote",
             () -> new StorageRemoteItem(defaultProperties().maxStackSize(1)));
@@ -71,9 +76,11 @@ public class OccultismItems {
     //Books of Calling
     //Foliot
     public static final RegistryObject<BookOfCallingItem> BOOK_OF_CALLING_FOLIOT_LUMBERJACK = ITEMS.register(
-            "book_of_calling_foliot_lumberjack", () -> new BookOfCallingItem(defaultProperties().maxStackSize(1), "book_of_calling_foliot"));
+            "book_of_calling_foliot_lumberjack",
+            () -> new BookOfCallingItem(defaultProperties().maxStackSize(1), "book_of_calling_foliot"));
     public static final RegistryObject<BookOfCallingItem> BOOK_OF_CALLING_FOLIOT_MANAGE_MACHINE = ITEMS.register(
-            "book_of_calling_foliot_manage_machine", () -> new BookOfCallingItem(defaultProperties().maxStackSize(1), "book_of_calling_foliot"));
+            "book_of_calling_foliot_manage_machine",
+            () -> new BookOfCallingItem(defaultProperties().maxStackSize(1), "book_of_calling_foliot"));
     //Djinn
 
     //Machines
