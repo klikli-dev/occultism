ccc11dec5d563a18242f5f915c98b11346efd2e5
klikli-dev <klikli@outlook.at>
2020-03-08 15:00:53 +0100

    added spirit fire creation via flint and steel and datura

---
+++ src/main/java/com/github/klikli_dev/occultism/handlers/PlayerEventHandler.java
@@ -0,0 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright 2020 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.klikli_dev.occultism.handlers;
+
+import com.github.klikli_dev.occultism.Occultism;
+import com.github.klikli_dev.occultism.registry.OccultismBlocks;
+import com.github.klikli_dev.occultism.registry.OccultismItems;
+import com.github.klikli_dev.occultism.util.Math3DUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = Occultism.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class PlayerEventHandler {
+    //region Static Methods
+    @SubscribeEvent
+    public static void onPlayerRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (event.getItemStack().getItem() == Items.FLINT_AND_STEEL) {
+            AxisAlignedBB box = new AxisAlignedBB(-1, -1, -1, 1, 1, 1)
+                                        .offset(Math3DUtil.center(event.getPos()));
+            List<ItemEntity> list = event.getWorld().getEntitiesWithinAABB(ItemEntity.class, box,
+                    item -> item.getItem().getItem() == OccultismItems.DATURA.get());
+            if (!list.isEmpty()) {
+                BlockPos pos = event.getPos().offset(event.getFace());
+                if (!event.getPlayer().canPlayerEdit(pos, event.getFace(), event.getItemStack())) {
+                    return;
+                }
+
+                list.forEach(Entity::remove);
+                World world = event.getWorld();
+                if (world.isAirBlock(pos)) {
+                    world.playSound(event.getPlayer(), pos, SoundEvents.ITEM_FLINTANDSTEEL_USE,
+                            SoundCategory.BLOCKS, 1.0F, world.rand.nextFloat() * 0.4F + 0.8F);
+                    world.setBlockState(pos, OccultismBlocks.SPIRIT_FIRE.get().getDefaultState(), 11);
+                }
+                event.getItemStack().damageItem(1, event.getPlayer(), (player) -> {
+                    player.sendBreakAnimation(event.getHand());
+                });
+                event.setCanceled(true);
+                event.getPlayer().swingArm(Hand.MAIN_HAND);
+            }
+        }
+    }
+    //endregion Static Methods
+    //region Methods
+    //endregion Methods
+}
