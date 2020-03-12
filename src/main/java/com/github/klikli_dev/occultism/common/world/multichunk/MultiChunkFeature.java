2b4c9e37b75d9090376f0eb191e6781b47d113fd
klikli-dev <klikli@outlook.at>
2020-03-12 16:59:01 +0100

    added multi chunk feature

---
+++ src/main/java/com/github/klikli_dev/occultism/common/world/multichunk/MultiChunkFeature.java
@@ -0,0 +1,100 @@
+/*
+ * MIT License
+ *
+ * Copyright 2020 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.klikli_dev.occultism.common.world.multichunk;
+
+import com.github.klikli_dev.occultism.util.Math3DUtil;
+import com.mojang.datafixers.Dynamic;
+import net.minecraft.util.SharedSeedRandom;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationSettings;
+import net.minecraft.world.gen.feature.Feature;
+
+import java.util.Random;
+import java.util.function.Function;
+
+public abstract class MultiChunkFeature<T extends MultiChunkFeatureConfig> extends Feature<T> {
+
+    //region Fields
+    public final IMultiChunkSubFeature subFeature;
+    //endregion Fields
+
+    //region Initialization
+    public MultiChunkFeature(Function<Dynamic<?>, ? extends T> configFactoryIn, IMultiChunkSubFeature subFeature) {
+        super(configFactoryIn);
+        this.subFeature = subFeature;
+    }
+    //endregion Initialization
+
+    //region Getter / Setter
+    public abstract long getFeatureSeed();
+    //endregion Getter / Setter
+
+    //region Overrides
+    @Override
+    public boolean place(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generator, Random rand,
+                         BlockPos pos, T config) {
+        //If dimensions are restricted and this dimension is not allowed, exit
+        if (!config.allowedDimensionTypes.contains(worldIn.getDimension().getType()) &&
+            !config.allowedDimensionTypes.isEmpty()) {
+            return false;
+        }
+        ChunkPos generatingChunk = new ChunkPos(pos);
+        BlockPos rootPosition = this.getRootPosition(generator, (SharedSeedRandom) rand, generatingChunk, config);
+        //If no root position was found in range, we exit
+        if (rootPosition == null) {
+            return false;
+        }
+
+        return this.subFeature.place(worldIn, generator, rand, pos, rootPosition,
+                Math3DUtil.bounds(generatingChunk, generator.getMaxHeight()));
+    }
+    //endregion Overrides
+
+    //region Methods
+    protected BlockPos getRootPosition(ChunkGenerator<?> generator, SharedSeedRandom random, ChunkPos generatingChunk,
+                                       T config) {
+        for (int i = -config.maxChunksToRoot; i < config.maxChunksToRoot; i++) {
+            for (int j = -config.maxChunksToRoot; j < config.maxChunksToRoot; i++) {
+
+                ChunkPos currentChunk = new ChunkPos(generatingChunk.x + i, generatingChunk.z + j);
+                //Seed random for this chunk, this way we get the same result no matter how often this is called.
+                random.setLargeFeatureSeed(this.getFeatureSeed(), currentChunk.x, currentChunk.z);
+
+                if (random.nextInt(config.chanceToGenerate) == 0) {
+                    //this chunk contains a root, so we generate a random
+                    return currentChunk.getBlock(
+                            random.nextInt(15),
+                            random.nextInt(generator.getMaxHeight()),
+                            random.nextInt(15)
+                    );
+                }
+            }
+        }
+        return null;
+    }
+    //endregion Methods
+
+}
