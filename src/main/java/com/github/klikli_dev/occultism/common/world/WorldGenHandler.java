d0ee340a9f02262151be5e62d010bd41a02f8c23
klikli-dev <klikli@outlook.at>
2020-03-14 13:55:48 +0100

    generified ore settings

--- src/main/java/com/github/klikli_dev/occultism/common/world/WorldGenHandler.java
+++ src/main/java/com/github/klikli_dev/occultism/common/world/WorldGenHandler.java
@@ -55,7 +55,7 @@ public class WorldGenHandler {
                     Collectors.toList());
 
     protected static final List<DimensionType> OTHERSTONE_DIMENSION_WHITELIST =
-            Occultism.CONFIG.worldGen.oreGen.dimensionTypeWhitelist.get().stream()
+            Occultism.CONFIG.worldGen.oreGen.otherstoneNatural.dimensionTypeWhitelist.get().stream()
                     .map(s -> DimensionType.byName(new ResourceLocation(s))).collect(
                     Collectors.toList());
     //endregion Fields
@@ -63,18 +63,18 @@ public class WorldGenHandler {
     //region Static Methods
     public static void setupOreGeneration() {
         for (Biome biome : ForgeRegistries.BIOMES) {
-            if (Occultism.CONFIG.worldGen.oreGen.otherstoneOreChance.get() > 0) {
+            if (Occultism.CONFIG.worldGen.oreGen.otherstoneNatural.oreChance.get() > 0) {
                 biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
                         OccultismBiomeFeatures.DIMENSION_ORE_FEATURE.get()
                                 .withConfiguration(new DimensionOreFeatureConfig(OTHERSTONE_DIMENSION_WHITELIST,
                                         OreFeatureConfig.FillerBlockType.NATURAL_STONE,
                                         OccultismBlocks.OTHERSTONE_NATURAL.get().getDefaultState(),
-                                        Occultism.CONFIG.worldGen.oreGen.otherstoneOreSize.get()))
+                                        Occultism.CONFIG.worldGen.oreGen.otherstoneNatural.oreSize.get()))
                                 .withPlacement(Placement.COUNT_RANGE.configure(
                                         new CountRangeConfig(
-                                                Occultism.CONFIG.worldGen.oreGen.otherstoneOreChance.get(),
-                                                Occultism.CONFIG.worldGen.oreGen.otherstoneOreMin.get(), 0,
-                                                Occultism.CONFIG.worldGen.oreGen.otherstoneOreMax.get()))));
+                                                Occultism.CONFIG.worldGen.oreGen.otherstoneNatural.oreChance.get(),
+                                                Occultism.CONFIG.worldGen.oreGen.otherstoneNatural.oreMin.get(), 0,
+                                                Occultism.CONFIG.worldGen.oreGen.otherstoneNatural.oreMax.get()))));
             }
         }
     }
