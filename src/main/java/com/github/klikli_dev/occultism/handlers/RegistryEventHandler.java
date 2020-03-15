a63c20092bbeb3630bb94a8fd641652b6af8f2c4
klikli-dev <klikli@outlook.at>
2020-03-15 22:03:12 +0100

    moved spawn egg models into their own asset folder

--- src/main/java/com/github/klikli_dev/occultism/handlers/RegistryEventHandler.java
+++ src/main/java/com/github/klikli_dev/occultism/handlers/RegistryEventHandler.java
@@ -79,16 +79,16 @@ public class RegistryEventHandler {
         });
         Occultism.LOGGER.info("Registered BlockItems");
 
-        registerSpawnEgg(registry, OccultismEntities.FOLIOT_TYPE.get(), modLoc("spawn_egg.foliot"), 0xaa728d, 0x37222c);
-        registerSpawnEgg(registry, OccultismEntities.POSSESSED_ENDERMITE_TYPE.get(), modLoc("spawn_egg.possessed_endermite"), 0x161616, 0x6E6E6E);
+        registerSpawnEgg(registry, OccultismEntities.FOLIOT_TYPE.get(), "foliot", 0xaa728d, 0x37222c);
+        registerSpawnEgg(registry, OccultismEntities.POSSESSED_ENDERMITE_TYPE.get(),"possessed_endermite", 0x161616, 0x6E6E6E);
         Occultism.LOGGER.info("Registered SpawnEggItems");
     }
 
     public static void registerSpawnEgg(IForgeRegistry<Item> registry, EntityType<?> entityType,
-                                        ResourceLocation registryName, int primaryColor, int secondaryColor) {
+                                        String name, int primaryColor, int secondaryColor) {
         SpawnEggItem spawnEggItem = new SpawnEggItem(entityType, primaryColor, secondaryColor,
                 new Item.Properties().group(Occultism.ITEM_GROUP));
-        spawnEggItem.setRegistryName(registryName);
+        spawnEggItem.setRegistryName(modLoc("spawn_egg/" + name));
         registry.register(spawnEggItem);
     }
     //endregion Static Methods
