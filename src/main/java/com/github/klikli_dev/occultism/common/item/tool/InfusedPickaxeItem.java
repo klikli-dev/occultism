ef63a3d0abefffa6089486fa3b863de9a4ec958e
klikli-dev <klikli@outlook.at>
2020-03-20 10:58:33 +0100

    infused pickaxe now shows spirit name

--- src/main/java/com/github/klikli_dev/occultism/common/item/tool/InfusedPickaxeItem.java
+++ src/main/java/com/github/klikli_dev/occultism/common/item/tool/InfusedPickaxeItem.java
@@ -24,18 +24,40 @@ package com.github.klikli_dev.occultism.common.item.tool;
 
 import com.github.klikli_dev.occultism.api.common.data.OtherworldBlockTier;
 import com.github.klikli_dev.occultism.api.common.item.IOtherworldTool;
+import com.github.klikli_dev.occultism.util.ItemNBTUtil;
+import com.github.klikli_dev.occultism.util.TextUtil;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class InfusedPickaxeItem extends PickaxeItem implements IOtherworldTool {
+    //region Initialization
     public InfusedPickaxeItem(IItemTier tier, int attackDamageIn, float attackSpeedIn,
                               Properties builder) {
         super(tier, attackDamageIn, attackSpeedIn, builder);
     }
+    //endregion Initialization
 
+    //region Overrides
     @Override
     public OtherworldBlockTier getHarvestTier(ItemStack stack) {
         return OtherworldBlockTier.TWO;
     }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip,
+                               ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        tooltip.add(new TranslationTextComponent(this.getTranslationKey() + ".tooltip",
+                TextUtil.formatDemonName(ItemNBTUtil.getBoundSpiritName(stack))));
+    }
+    //endregion Overrides
+
 }
