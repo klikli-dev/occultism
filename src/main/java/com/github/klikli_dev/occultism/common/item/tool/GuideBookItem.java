163880540ba51d1d530ff518c31f40b882f964de
klikli-dev <klikli@outlook.at>
2020-05-02 13:25:57 +0200

    now using API to open book gui

--- src/main/java/com/github/klikli_dev/occultism/common/item/tool/GuideBookItem.java
+++ src/main/java/com/github/klikli_dev/occultism/common/item/tool/GuideBookItem.java
@@ -30,7 +30,10 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.*;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -38,12 +41,10 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import vazkii.patchouli.api.PatchouliAPI;
 import vazkii.patchouli.client.book.BookEntry;
-import vazkii.patchouli.common.base.PatchouliSounds;
 import vazkii.patchouli.common.book.Book;
 import vazkii.patchouli.common.book.BookRegistry;
-import vazkii.patchouli.common.network.NetworkHandler;
-import vazkii.patchouli.common.network.message.MessageOpenBookGui;
 
 import javax.annotation.Nullable;
 import java.util.Iterator;
@@ -91,20 +92,10 @@ public class GuideBookItem extends Item {
 
     //region Overrides
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-        ItemStack stack = playerIn.getHeldItem(handIn);
-        Book book = BookRegistry.INSTANCE.books.get(GUIDE);
-        if (book == null) {
-            return new ActionResult<>(ActionResultType.FAIL, stack);
+        if (!worldIn.isRemote) {
+            PatchouliAPI.instance.openBookGUI((ServerPlayerEntity) playerIn, GUIDE);
         }
-        else {
-            if (playerIn instanceof ServerPlayerEntity) {
-                NetworkHandler.sendToPlayer(new MessageOpenBookGui(book.id), (ServerPlayerEntity) playerIn);
-                SoundEvent sfx = PatchouliSounds.getSound(book.openSound, PatchouliSounds.book_open);
-                playerIn.playSound(sfx, 1.0F, (float) (0.7D + Math.random() * 0.4D));
-            }
-
-            return new ActionResult<>(ActionResultType.SUCCESS, stack);
-        }
+        return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
     }
 
     @OnlyIn(Dist.CLIENT)
