85784998360d5efee31bbdf52a7d525eb25b21e2
klikli-dev <klikli@outlook.at>
2020-05-06 19:18:53 +0200

    fixed nbt serialization of item stacks in the storage actuator

--- src/main/java/com/github/klikli_dev/occultism/common/misc/StorageControllerItemStackHandler.java
+++ src/main/java/com/github/klikli_dev/occultism/common/misc/StorageControllerItemStackHandler.java
@@ -25,6 +25,9 @@ package com.github.klikli_dev.occultism.common.misc;
 import com.github.klikli_dev.occultism.api.common.tile.IStorageController;
 import com.github.klikli_dev.occultism.common.data.NonNullArrayList;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
@@ -100,8 +103,44 @@ public class StorageControllerItemStackHandler extends ItemStackHandler {
     }
 
     @Override
+    public CompoundNBT serializeNBT() {
+        ListNBT nbtTagList = new ListNBT();
+        for (int i = 0; i < this.stacks.size(); i++) {
+            if (!this.stacks.get(i).isEmpty()) {
+                CompoundNBT itemTag = new CompoundNBT();
+                ItemStack stack = this.stacks.get(i);
+                itemTag.putInt("Slot", i);
+                stack.write(itemTag);
+                itemTag.putInt("RealSize", stack.getCount());
+                nbtTagList.add(itemTag);
+            }
+        }
+        CompoundNBT nbt = new CompoundNBT();
+        nbt.put("Items", nbtTagList);
+        nbt.putInt("Size", this.stacks.size());
+        return nbt;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        this.setSize(nbt.contains("Size", Constants.NBT.TAG_INT) ? nbt.getInt("Size") : this.stacks.size());
+        ListNBT tagList = nbt.getList("Items", Constants.NBT.TAG_COMPOUND);
+        for (int i = 0; i < tagList.size(); i++) {
+            CompoundNBT itemTags = tagList.getCompound(i);
+            int slot = itemTags.getInt("Slot");
+
+            if (slot >= 0 && slot < this.stacks.size()) {
+                ItemStack stack = ItemStack.read(itemTags);
+                stack.setCount(itemTags.getInt("RealSize"));
+                this.stacks.set(slot, stack);
+            }
+        }
+        this.onLoad();
+    }
+
+    @Override
     protected void onContentsChanged(int slot) {
-         this.storageController.onContentsChanged();
+        this.storageController.onContentsChanged();
     }
 
     //region Methods
