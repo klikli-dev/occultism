667fffa5167646379face18cf4d1a52af2e31e68
klikli-dev <klikli@outlook.at>
2021-01-29 17:22:59 +0100

    Enable curio slots for satchel and goggles

--- src/main/java/com/github/klikli_dev/occultism/Occultism.java
+++ src/main/java/com/github/klikli_dev/occultism/Occultism.java
@@ -37,14 +37,19 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+import top.theillusivec4.curios.api.SlotTypePreset;
 import vazkii.patchouli.api.PatchouliAPI;
 
 import static com.github.klikli_dev.occultism.util.StaticUtil.modLoc;
@@ -90,6 +95,7 @@ public class Occultism {
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::serverSetup);
         modEventBus.addListener(this::onModConfigEvent);
+        modEventBus.addListener(this::enqueueIMC);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -129,6 +135,11 @@ public class Occultism {
 
     private void serverSetup(final FMLDedicatedServerSetupEvent event) {
         LOGGER.info("Dedicated server setup complete.");
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event){
+        InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BACK.getMessageBuilder().build());
+        InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.HEAD.getMessageBuilder().build());
     }
     //endregion Methods
 }
