405380e33ec1e4efa9a7ca02e4d2ccecbeb7ea20
vemerion <vemerion@gmail.com>
2021-06-21 12:40:58 +0200

    Add Greedy Familiar spawn egg

--- src/main/java/com/github/klikli_dev/occultism/common/entity/GreedyFamiliarEntity.java
+++ src/main/java/com/github/klikli_dev/occultism/common/entity/GreedyFamiliarEntity.java
@@ -2,9 +2,14 @@ package com.github.klikli_dev.occultism.common.entity;
 
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
 
 public class GreedyFamiliarEntity extends CreatureEntity {
@@ -12,9 +17,14 @@ public class GreedyFamiliarEntity extends CreatureEntity {
     public GreedyFamiliarEntity(EntityType<? extends GreedyFamiliarEntity> type, World worldIn) {
         super(type, worldIn);
     }
-    
+
     public static AttributeModifierMap.MutableAttribute registerAttributes() {
         return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 6)
                 .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2);
+    }
+
+    @Override
+    public boolean canDespawn(double distanceToClosestPlayer) {
+        return false;
     }
 }
