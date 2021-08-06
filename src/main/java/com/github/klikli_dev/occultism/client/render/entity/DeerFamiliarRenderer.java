40191983036959d0c37e85955066f7b3860e553c
vemerion <vemerion@gmail.com>
2021-08-06 11:51:56 +0200

    Add Rudolph easter egg

--- src/main/java/com/github/klikli_dev/occultism/client/render/entity/DeerFamiliarRenderer.java
+++ src/main/java/com/github/klikli_dev/occultism/client/render/entity/DeerFamiliarRenderer.java
@@ -25,9 +25,16 @@ package com.github.klikli_dev.occultism.client.render.entity;
 import com.github.klikli_dev.occultism.Occultism;
 import com.github.klikli_dev.occultism.client.model.entity.DeerFamiliarModel;
 import com.github.klikli_dev.occultism.common.entity.DeerFamiliarEntity;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.IEntityRenderer;
+import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.util.ResourceLocation;
 
 public class DeerFamiliarRenderer extends MobRenderer<DeerFamiliarEntity, DeerFamiliarModel> {
@@ -35,12 +42,45 @@ public class DeerFamiliarRenderer extends MobRenderer<DeerFamiliarEntity, DeerFa
     private static final ResourceLocation TEXTURES = new ResourceLocation(Occultism.MODID,
             "textures/entity/deer_familiar.png");
 
+    private static final int MAX_LIGHT = 15728880;
+
     public DeerFamiliarRenderer(EntityRendererManager renderManagerIn) {
         super(renderManagerIn, new DeerFamiliarModel(), 0.3f);
+        layerRenderers.add(new RedNoseLayer(this));
+    }
+
+    @Override
+    public void render(DeerFamiliarEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn,
+            IRenderTypeBuffer bufferIn, int packedLightIn) {
+        super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn,
+                entityIn.hasRedNose() ? MAX_LIGHT : packedLightIn);
     }
 
     @Override
     public ResourceLocation getEntityTexture(DeerFamiliarEntity entity) {
         return TEXTURES;
+    }
+
+    private static class RedNoseLayer extends LayerRenderer<DeerFamiliarEntity, DeerFamiliarModel> {
+
+        private static final ResourceLocation RED_NOSE = new ResourceLocation(Occultism.MODID,
+                "textures/entity/deer_familiar_red_nose.png");
+
+        public RedNoseLayer(IEntityRenderer<DeerFamiliarEntity, DeerFamiliarModel> entityRendererIn) {
+            super(entityRendererIn);
+        }
+
+        @Override
+        public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn,
+                DeerFamiliarEntity deer, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks,
+                float netHeadYaw, float headPitch) {
+            if (deer.isInvisible() || !deer.hasRedNose())
+                return;
+
+            DeerFamiliarModel model = this.getEntityModel();
+            IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.getEntityCutout(RED_NOSE));
+            model.render(matrixStackIn, ivertexbuilder, MAX_LIGHT, LivingRenderer.getPackedOverlay(deer, 0), 1, 1, 1,
+                    1);
+        }
     }
 }
--- src/main/java/com/github/klikli_dev/occultism/common/entity/DeerFamiliarEntity.java
+++ src/main/java/com/github/klikli_dev/occultism/common/entity/DeerFamiliarEntity.java
@@ -25,10 +25,20 @@ package com.github.klikli_dev.occultism.common.entity;
 import com.google.common.collect.ImmutableList;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
 
 public class DeerFamiliarEntity extends FamiliarEntity {
+    
+    private static final DataParameter<Boolean> RED_NOSE = EntityDataManager.createKey(DeerFamiliarEntity.class, DataSerializers.BOOLEAN);
 
     public DeerFamiliarEntity(EntityType<? extends DeerFamiliarEntity> type, World worldIn) {
         super(type, worldIn);
@@ -38,5 +48,37 @@ public class DeerFamiliarEntity extends FamiliarEntity {
     public Iterable<EffectInstance> getFamiliarEffects() {
         return ImmutableList.of();
     }
+    
+    @Override
+    protected void registerData() {
+        super.registerData();
+        this.dataManager.register(RED_NOSE, false);
+    }
 
+    @Override
+    public ILivingEntityData onInitialSpawn(IServerWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason,
+            ILivingEntityData spawnDataIn, CompoundNBT dataTag) {
+        this.setRedNose(this.getRNG().nextDouble() < 0.1);
+        return super.onInitialSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
+    }
+
+    @Override
+    public void readAdditional(CompoundNBT compound) {
+        super.readAdditional(compound);
+        this.setRedNose(compound.getBoolean("hasRedNose"));
+    }
+
+    @Override
+    public void writeAdditional(CompoundNBT compound) {
+        super.writeAdditional(compound);
+        compound.putBoolean("hasRedNose", this.hasRedNose());
+    }
+
+    public boolean hasRedNose() {
+        return this.dataManager.get(RED_NOSE);
+    }
+    
+    private void setRedNose(boolean b) {
+        this.dataManager.set(RED_NOSE, b);
+    }
 }
