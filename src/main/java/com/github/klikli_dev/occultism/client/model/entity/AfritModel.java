06208a9217edef96ca2b6c95c43a93b0e7f5256a
Kli Kli <klikli@outlook.at>
2021-08-20 15:33:58 +0200

    Convert afrit to new model format

--- src/main/java/com/github/klikli_dev/occultism/client/model/entity/AfritModel.java
+++ src/main/java/com/github/klikli_dev/occultism/client/model/entity/AfritModel.java
@@ -24,12 +24,9 @@ package com.github.klikli_dev.occultism.client.model.entity;
 
 import com.github.klikli_dev.occultism.common.entity.spirit.AfritEntity;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.VillagerModel;
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.resources.ResourceLocation;
 
 
 public class AfritModel extends HumanoidModel<AfritEntity> {
@@ -63,75 +60,6 @@ public class AfritModel extends HumanoidModel<AfritEntity> {
         this.wingedWings = this.body.getChild(WINGED_WINGS);
         this.wingedWingsLeft = this.wingedWings.getChild(WINGED_WINGS_LEFT);
         this.wingedWingsRight = this.wingedWings.getChild(WINGED_WINGS_RIGHT);
-
-        //This no longer works!
-        //TODO: Update model construction to 1.17 way
-        //      search for "addbox" in 117 channel
-        //      or just open https://discordapp.com/channels/313125603924639766/867851603468615740/868187284661469184
-        //      or browse https://gist.github.com/gigaherz/7115024820f55717bc40a6e2247c6aca
-        //      also look at e.g. parrot: ParrotModel, ParrotOnShoulderLayer, Modellayers -> yes we need modellayers for the entity themselves too!
-
-        this.earLeft = new ModelPart(this, 0, 0);
-        this.earLeft.setPos(4.0F, -11.3F, 3.5F);
-        this.earLeft.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1, 0.0F);
-        this.setRotateAngle(this.earLeft, -0.5009094953223726F, 0.0F, 0.0F);
-
-        this.earRight = new ModelPart(this, 0, 0);
-        this.earRight.setPos(-4.0F, -11.3F, 3.5F);
-        this.earRight.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1, 0.0F);
-        this.setRotateAngle(this.earRight, -0.5009094953223726F, 0.0F, 0.0F);
-
-
-
-        this.bipedLeftArm = new ModelPart(this, 44, 22);
-        this.bipedLeftArm.setPos(5.0F, 3.0F, -1.0F);
-        this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 11, 4, 0.0F);
-        this.setRotateAngle(this.bipedLeftArm, -0.7499679795819634F, 0.0F, 0.0F);
-
-        this.bipedRightArm = new ModelPart(this, 44, 22);
-        this.bipedRightArm.setPos(-5.0F, 3.0F, -1.0F);
-        this.bipedRightArm.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
-        this.setRotateAngle(this.bipedRightArm, -0.7499679795819634F, 0.0F, 0.0F);
-
-        this.bipedBody = new ModelPart(this, 16, 20);
-        this.bipedBody.setPos(0.0F, 0.0F, 0.0F);
-        this.bipedBody.addBox(-4.0F, 0.0F, -3.0F, 8, 12, 6, 0.0F);
-
-        this.bipedLeftLeg = new ModelPart(this, 0, 22);
-        this.bipedLeftLeg.mirror = true;
-        this.bipedLeftLeg.setPos(2.0F, 12.0F, 0.0F);
-        this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
-
-        this.bipedRightLeg = new ModelPart(this, 0, 22);
-        this.bipedRightLeg.setPos(-2.0F, 12.0F, 0.0F);
-        this.bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, 0.0F);
-
-        this.head.addChild(this.earRight);
-        this.head.addChild(this.earLeft);
-        this.head.addChild(this.nose);
-
-        this.wingedWings = new ModelPart(this);
-        this.wingedWings.setPos(0.0F, 24.0F, 0.0F);
-        this.bipedBody.addChild(this.wingedWings);
-
-        this.wingedWingsLeft = new ModelPart(this);
-        this.wingedWingsLeft.setPos(1.5F, -21.5F, 3.0F);
-        this.setRotateAngle(this.wingedWingsLeft, 0.4363F, 0.8727F, 0.0F);
-        this.wingedWings.addChild(this.wingedWingsLeft);
-        this.wingedWingsLeft.setTextureOffset(40, 38);
-        this.wingedWingsLeft.addBox(-1.0F, -1.0F, 0.0F, 1, 1, 11);
-        this.wingedWingsLeft.setTextureOffset(32, 0);
-        this.wingedWingsLeft.addBox(-0.1F, 0.0F, -0.4226F, 0, 9, 11);
-
-        this.wingedWingsRight = new ModelPart(this);
-        this.wingedWingsRight.setPos(-0.5F, -21.5F, 3.0F);
-        this.setRotateAngle(this.wingedWingsRight, 0.4363F, -0.8727F, 0.0F);
-        this.wingedWings.addChild(this.wingedWingsRight);
-        this.wingedWingsRight.setTextureOffset(40, 38);
-        this.wingedWingsRight.addBox( -1.0F, -1.0F, 0.0F, 1, 1, 11);
-        this.wingedWingsRight.setTextureOffset(32, 0);
-        this.wingedWingsRight.addBox(-0.1F, 0.0F, -0.4226F, 0, 9, 11);
-
     }
     //endregion Initialization
 
@@ -144,24 +72,72 @@ public class AfritModel extends HumanoidModel<AfritEntity> {
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
-        PartDefinition partdefinition = meshdefinition.getRoot();
+        PartDefinition mesh = meshdefinition.getRoot();
 
-        // new ModelPart(this, texOffsX, texOffsY) -> texOffs()
-        // setPos -> PartPose.offset
-        // addBox -> addBox
-        // addBox(..., delta) -> addBox(..., CubeDeformation)
-        //      delta is also weirdly used as model size in vanilla (+ part delta)
-        // x.addChild -> partDefintion.addorReplaceChild()
-        //      also needs setup in constructor:    this.nose = this.head.getChild("nose");
-        PartDefinition head = partdefinition.addOrReplaceChild("head",
-                CubeListBuilder.create().texOffs(0, 0)
-                        .addBox(-4.0F, -10.0F, -4.0F, 8.0F, 10.0F, 8.0F, CubeDeformation.NONE), PartPose.ZERO);
-        partdefinition.addOrReplaceChild("hat",
-                CubeListBuilder.create().texOffs(1, 45)
-                        .addBox(-4.0F, -10.0F, -4.0F, 8.0F, 10.0F, 8.0F, new CubeDeformation(0.5f)), PartPose.offset(0, -3, 0));
+        PartDefinition head = mesh.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -10.0F, -4.0F, 8.0F, 10.0F, 8.0F, CubeDeformation.NONE), PartPose.ZERO);
+        mesh.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(1, 45).addBox(-4.0F, -10.0F, -4.0F, 8.0F, 10.0F, 8.0F, new CubeDeformation(0.5f)), PartPose.offset(0, -3, 0));
 
+        head.addOrReplaceChild(NOSE, CubeListBuilder.create().texOffs(24, 0)
+                        .addBox(-1.0F, 0.0F, 0.0F, 2, 4, 2,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(0.0F, -5.0F, -4.4F, -0.4553564018453205F, 0.0F, 0.0F));
 
-        return LayerDefinition.create(meshdefinition, 64, 64);;
+        head.addOrReplaceChild(EAR_LEFT, CubeListBuilder.create().texOffs(0, 0)
+                        .addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(4.0F, -11.3F, 3.5F, -0.5009094953223726F, 0.0F, 0.0F));
+
+        head.addOrReplaceChild(EAR_RIGHT, CubeListBuilder.create().texOffs(0, 0)
+                        .addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(-4.0F, -11.3F, 3.5F, -0.5009094953223726F, 0.0F, 0.0F));
+
+        mesh.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(44, 22)
+                        .addBox(-1.0F, -2.0F, -2.0F, 4, 11, 4,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(5.0F, 3.0F, -1.0F, -0.7499679795819634F, 0.0F, 0.0F));
+
+        mesh.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(44, 22)
+                        .addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(-5.0F, 3.0F, -1.0F, -0.7499679795819634F, 0.0F, 0.0F));
+
+        PartDefinition body = mesh.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 20)
+                        .addBox(-4.0F, 0.0F, -3.0F, 8, 12, 6,
+                                CubeDeformation.NONE),
+                PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        mesh.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 22)
+                        .addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4,
+                                CubeDeformation.NONE).mirror(),
+                PartPose.offset(2.0F, 12.0F, 0.0F));
+
+        mesh.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 22)
+                        .addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4,
+                                CubeDeformation.NONE),
+                PartPose.offset(-2.0F, 12.0F, 0.0F));
+
+        PartDefinition wingedWings = body.addOrReplaceChild(WINGED_WINGS, CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+        wingedWings.addOrReplaceChild(WINGED_WINGS_LEFT, CubeListBuilder.create()
+                        .texOffs(40, 38)
+                        .addBox(-1.0F, -1.0F, 0.0F, 1, 1, 11,
+                                CubeDeformation.NONE)
+                        .texOffs(32, 0)
+                        .addBox(-0.1F, 0.0F, -0.4226F, 0, 9, 11,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(1.5F, -21.5F, 3.0F, 0.4363F, 0.8727F, 0.0F));
+
+        wingedWings.addOrReplaceChild(WINGED_WINGS_RIGHT, CubeListBuilder.create()
+                        .texOffs(40, 38)
+                        .addBox(-1.0F, -1.0F, 0.0F, 1, 1, 11,
+                                CubeDeformation.NONE)
+                        .texOffs(32, 0)
+                        .addBox(-0.1F, 0.0F, -0.4226F, 0, 9, 11,
+                                CubeDeformation.NONE),
+                PartPose.offsetAndRotation(-0.5F, -21.5F, 3.0F, 0.4363F, -0.8727F, 0.0F));
+
+        return LayerDefinition.create(meshdefinition, 64, 64);
     }
     //endregion Methods
 }
