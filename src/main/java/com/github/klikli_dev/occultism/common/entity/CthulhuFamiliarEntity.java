55263a8e87570a6b23a362bbea886925604f075f
vemerion <vemerion@gmail.com>
2021-08-22 10:50:52 +0200

    Add dapper cthulhu easter egg

--- src/main/java/com/github/klikli_dev/occultism/common/entity/CthulhuFamiliarEntity.java
+++ src/main/java/com/github/klikli_dev/occultism/common/entity/CthulhuFamiliarEntity.java
@@ -25,13 +25,31 @@ package com.github.klikli_dev.occultism.common.entity;
 import com.google.common.collect.ImmutableList;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
 
 public class CthulhuFamiliarEntity extends FamiliarEntity {
 
+    private static final DataParameter<Boolean> HAT = EntityDataManager.createKey(CthulhuFamiliarEntity.class,
+            DataSerializers.BOOLEAN);
+
     public CthulhuFamiliarEntity(EntityType<? extends CthulhuFamiliarEntity> type, World worldIn) {
         super(type, worldIn);
+    }
+
+    @Override
+    public ILivingEntityData onInitialSpawn(IServerWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason,
+            ILivingEntityData spawnDataIn, CompoundNBT dataTag) {
+        this.setHat(this.getRNG().nextDouble() < 0.1);
+        return super.onInitialSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
     }
 
     @Override
@@ -42,5 +60,31 @@ public class CthulhuFamiliarEntity extends FamiliarEntity {
     @Override
     public Iterable<EffectInstance> getFamiliarEffects() {
         return ImmutableList.of();
+    }
+
+    @Override
+    protected void registerData() {
+        super.registerData();
+        this.dataManager.register(HAT, false);
+    }
+
+    public boolean hasHat() {
+        return this.dataManager.get(HAT);
+    }
+
+    private void setHat(boolean b) {
+        this.dataManager.set(HAT, b);
+    }
+
+    @Override
+    public void readAdditional(CompoundNBT compound) {
+        super.readAdditional(compound);
+        this.setHat(compound.getBoolean("hasHat"));
+    }
+
+    @Override
+    public void writeAdditional(CompoundNBT compound) {
+        super.writeAdditional(compound);
+        compound.putBoolean("hasHat", this.hasHat());
     }
 }
