1d2f0a566c607595fcd4ffaa4b2e496268a3c208
Kli Kli <klikli@outlook.at>
2021-09-15 15:39:15 +0200

    Enable spirit tooltips on all items with nbt + translation key
    Closes #406
    Includes sample for diamond sword with nbt tag

---
+++ src/main/java/com/github/klikli_dev/occultism/handlers/TooltipHandler.java
@@ -0,0 +1,49 @@
+/*
+ * MIT License
+ *
+ * Copyright 2021 klikli-dev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.klikli_dev.occultism.handlers;
+
+import com.github.klikli_dev.occultism.Occultism;
+import com.github.klikli_dev.occultism.util.ItemNBTUtil;
+import com.github.klikli_dev.occultism.util.TextUtil;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Occultism.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class TooltipHandler {
+
+    @SubscribeEvent
+    public static void onAddInformation(ItemTooltipEvent event) {
+        ItemStack stack = event.getItemStack();
+        if (stack.hasTag() && stack.getTag().contains(ItemNBTUtil.SPIRIT_NAME_TAG)) {
+            String translationKey = stack.getTranslationKey() + ".occultism_spirit_tooltip";
+
+            if (I18n.hasKey(translationKey))
+                event.getToolTip().add(new TranslationTextComponent(translationKey,
+                        TextUtil.formatDemonName(ItemNBTUtil.getBoundSpiritName(stack))));
+        }
+    }
+}
