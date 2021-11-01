2b6cbbdcf1bfbe2401b5df8706d34d6dc75ac68b
Kli Kli <klikli@outlook.at>
2021-11-01 15:46:10 +0100

    Finalize ritual

--- src/main/java/com/github/klikli_dev/occultism/datagen/OccultismAdvancementProvider.java
+++ src/main/java/com/github/klikli_dev/occultism/datagen/OccultismAdvancementProvider.java
@@ -233,6 +233,7 @@ public class OccultismAdvancementProvider implements IDataProvider {
             this.addRitualHidden(root, "familiar_parrot");
             this.addRitualHidden(root, "familiar_guardian");
             this.addRitualHidden(root, "familiar_headless");
+            this.addRitualHidden(root, "familiar_chimera");
             this.addRitualHidden(root, "possess_enderman");
             this.addRitualHidden(root, "possess_endermite");
             this.addRitualHidden(root, "possess_skeleton");
