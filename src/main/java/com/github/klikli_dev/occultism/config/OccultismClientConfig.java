cdb0422e848bd25ff713190b4bff021b1e7db200
Kli Kli <klikli@outlook.at>
2022-03-20 17:56:18 +0100

    Set more ambitious default values
    Will allow people to find otherworld more easily
    See also #679 (which is not actually an issue, but will prevent this misconception)

--- src/main/java/com/github/klikli_dev/occultism/config/OccultismClientConfig.java
+++ src/main/java/com/github/klikli_dev/occultism/config/OccultismClientConfig.java
@@ -64,10 +64,10 @@ public class OccultismClientConfig {
 
             this.syncJeiSearch = builder.comment("Sync JEI search in storage actuator.")
                     .define("syncJeiSearch", false);
-            this.divinationRodHighlightAllResults = builder.comment("If true, divination rod will render all matching blocks with an outline. Keep disabled if it causes lag.")
+            this.divinationRodHighlightAllResults = builder.comment("If true, divination rod will render all matching blocks with an outline. Disable if it causes lag.")
                     .define("divinationRodHighlightAllResults", false);
             this.divinationRodScanRange = builder.comment("The scan range in blocks for the divination rod. Too high might cause lags")
-                    .defineInRange("divinationRodScanRange", 96, 1, Integer.MAX_VALUE);
+                    .defineInRange("divinationRodScanRange", 129, 1, Integer.MAX_VALUE);
             builder.pop();
         }
     }
