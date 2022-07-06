91efd8150a4e8c7584cbd9ecd54e35cd3ba291ed
Kli Kli <klikli@outlook.at>
2022-07-06 14:58:53 +0200

    Don't rely on Github copilot too much ...

--- src/main/java/com/github/klikli_dev/occultism/common/ritual/Ritual.java
+++ src/main/java/com/github/klikli_dev/occultism/common/ritual/Ritual.java
@@ -66,13 +66,13 @@ public abstract class Ritual {
      * The default range to listen for sacrifices.
      */
     public static final int SACRIFICE_DETECTION_RANGE = 8;
-    public static final int SACRIFICE_DETECTION_RANGE_SQUARE = (int)Math.pow(SACRIFICE_DETECTION_RANGE, 2);
+    public static final int SACRIFICE_DETECTION_RANGE_SQUARE = SACRIFICE_DETECTION_RANGE * SACRIFICE_DETECTION_RANGE;
 
     /**
      * The default range to listen for sacrifices.
      */
     public static final int ITEM_USE_DETECTION_RANGE = 16;
-    public static final int ITEM_USE_DETECTION_RANGE_SQUARE = (int)Math.pow(ITEM_USE_DETECTION_RANGE, 2);
+    public static final int ITEM_USE_DETECTION_RANGE_SQUARE = ITEM_USE_DETECTION_RANGE * ITEM_USE_DETECTION_RANGE;
 
     public RitualRecipe recipe;
 
