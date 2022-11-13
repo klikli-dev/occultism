8bf949295a17de02b1c6aad8a557a5e4927ad0d0
Kli Kli <klikli@outlook.at>
2022-11-13 11:03:29 +0100

    Close #837

--- src/main/java/com/github/klikli_dev/occultism/common/container/storage/StableWormholeContainer.java
+++ src/main/java/com/github/klikli_dev/occultism/common/container/storage/StableWormholeContainer.java
@@ -121,14 +121,22 @@ public class StableWormholeContainer extends StorageControllerContainerBase {
     public boolean stillValid(Player player) {
         if (this.storageController == null || this.stableWormhole == null)
             return false;
+
         Level level = this.stableWormhole.getLevel();
+        BlockPos wormholePosition = this.stableWormhole.getBlockPos();
+
+        //close container if wormhole block is destroyed
+        if (level.getBlockEntity(wormholePosition) != this.stableWormhole)
+            return false;
+
         //send stack updates on a slow tick while interacting
         if (!level.isClientSide && level.getGameTime() % 40 == 0) {
             OccultismPackets.sendTo((ServerPlayer) player, this.storageController.getMessageUpdateStacks());
             OccultismPackets.sendTo((ServerPlayer) player,
                     new MessageUpdateLinkedMachines(this.storageController.getLinkedMachines()));
         }
-        BlockPos wormholePosition = this.stableWormhole.getBlockPos();
+
+        //prevent player from interacting with the container if the controller is not in range
         return player.distanceToSqr(wormholePosition.getX() + 0.5D, wormholePosition.getY() + 0.5D,
                 wormholePosition.getZ() + 0.5D) <= 64.0D;
     }
