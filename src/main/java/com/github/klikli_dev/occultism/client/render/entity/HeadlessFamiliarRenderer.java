bc5b469fa52a0f90f9fedce82c2c16b3bf110bc5
Kli Kli <klikli@outlook.at>
2022-12-23 16:31:10 +0100

    fix: crash with Cthulhu head for headless familiar
    Closes #870

--- src/main/java/com/github/klikli_dev/occultism/client/render/entity/HeadlessFamiliarRenderer.java
+++ src/main/java/com/github/klikli_dev/occultism/client/render/entity/HeadlessFamiliarRenderer.java
@@ -308,12 +308,11 @@ public class HeadlessFamiliarRenderer extends MobRenderer<HeadlessFamiliarEntity
         }
     }
 
-    private static class CthulhuHeadModel extends SkullModel {
+    private static class CthulhuHeadModel extends SkullModelBase {
         private final CthulhuFamiliarModel model;
         protected final ModelPart head;
 
         public CthulhuHeadModel(ModelPart part) {
-            super(part);
             this.model = new CthulhuFamiliarModel(part);
             this.head = part.getChild("body").getChild("head");
         }
