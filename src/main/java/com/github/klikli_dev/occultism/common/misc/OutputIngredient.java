dc99d395e0b11e5ec54d81c35ea017a67364f1b1
Kli Kli <klikli@outlook.at>
2023-02-01 17:26:15 +0100

    feat: support tag output in crushing recipes

--- src/main/java/com/github/klikli_dev/occultism/common/misc/OutputIngredient.java
+++ src/main/java/com/github/klikli_dev/occultism/common/misc/OutputIngredient.java
@@ -13,16 +13,31 @@ import java.util.stream.Stream;
 public class OutputIngredient {
 
     protected Ingredient ingredient;
-    protected ItemStack[] itemStacks;
+
+    //While we take the item from the ingredient, we take the count and nbt form this
+    protected ItemStack outputStackInfo;
+    protected ItemStack cachedOutputStack;
+
 
     public OutputIngredient(Ingredient ingredient) {
+        this(ingredient, ItemStack.EMPTY);
+    }
+
+    /**
+     * Creates a new OutputIngredient
+     * @param ingredient the ingredient to source the output item from
+     * @param outputStackInfo the additional required stack info (count, nbt) will be read from this
+     */
+    public OutputIngredient(Ingredient ingredient, ItemStack outputStackInfo) {
         this.ingredient = ingredient;
+        this.outputStackInfo = outputStackInfo;
     }
 
     public ItemStack getStack() {
         //copied from Ingredient.dissolve, but modified to handle tag ingredient preferred items
-        if (this.itemStacks == null) {
-            this.itemStacks = Arrays.stream(this.ingredient.values).flatMap((value) -> {
+        if (this.cachedOutputStack == null) {
+
+            var itemStacks = Arrays.stream(this.ingredient.values).flatMap((value) -> {
                 if (value instanceof Ingredient.TagValue tagValue) {
                     if (AlmostUnifiedIntegration.isLoaded()) {
                         return Stream.of(AlmostUnifiedIntegration.getPreferredItemForTag(tagValue.tag));
@@ -30,8 +45,13 @@ public class OutputIngredient {
                 }
                 return value.getItems().stream();
             }).distinct().toArray(ItemStack[]::new);
+
+            var outputStack = itemStacks[0].copy();
+            outputStack.setCount(this.outputStackInfo.getCount());
+            outputStack.setTag(this.outputStackInfo.getTag());
+            this.cachedOutputStack = outputStack;
         }
-        return this.itemStacks[0];
+        return this.cachedOutputStack;
     }
 
     public Ingredient getIngredient() {
