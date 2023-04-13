ea2252c51c369b72b245658a36c73754bc407690
Kli Kli <klikli@outlook.at>
2023-04-13 15:03:31 +0200

    feat: update to new DatapackBuiltinEntriesProvider

--- src/main/java/com/github/klikli_dev/occultism/datagen/DataGenerators.java
+++ src/main/java/com/github/klikli_dev/occultism/datagen/DataGenerators.java
@@ -29,22 +29,17 @@ import com.github.klikli_dev.occultism.datagen.lang.PTBRProvider;
 import com.github.klikli_dev.occultism.datagen.lang.loot.OccultismBlockLoot;
 import com.github.klikli_dev.occultism.datagen.lang.loot.OccultismEntityLoot;
 import com.github.klikli_dev.occultism.datagen.worldgen.OccultismRegistries;
-import net.minecraft.Util;
-import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.data.registries.RegistriesDatapackGenerator;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.BiFunction;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
@@ -72,13 +67,9 @@ public class DataGenerators {
         generator.addProvider(event.includeClient(), new FRFRProvider(generator.getPackOutput()));
         generator.addProvider(event.includeClient(), new PTBRProvider(generator.getPackOutput()));
 
-        var holderLookup = CompletableFuture.supplyAsync(OccultismRegistries::createLookup, Util.backgroundExecutor());
-        generator.addProvider(event.includeServer(), bindRegistries(RegistriesDatapackGenerator::new, holderLookup));
+        event.getGenerator().addProvider(event.includeServer(),
+                (DataProvider.Factory<DatapackBuiltinEntriesProvider>) output ->
+                        new DatapackBuiltinEntriesProvider(output, event.getLookupProvider(), OccultismRegistries.BUILDER, Set.of(Occultism.MODID)));
     }
 
-    private static <T extends DataProvider> DataProvider.Factory<T> bindRegistries(BiFunction<PackOutput, CompletableFuture<HolderLookup.Provider>, T> datagenFactory, CompletableFuture<HolderLookup.Provider> lookupProvider) {
-        return (packOutput) -> {
-            return datagenFactory.apply(packOutput, lookupProvider);
-        };
-    }
 }
--- src/main/java/com/github/klikli_dev/occultism/datagen/worldgen/OccultismRegistries.java
+++ src/main/java/com/github/klikli_dev/occultism/datagen/worldgen/OccultismRegistries.java
@@ -10,17 +10,11 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class OccultismRegistries {
-    private static final RegistrySetBuilder BUILDER = (new RegistrySetBuilder())
+    public static final RegistrySetBuilder BUILDER = (new RegistrySetBuilder())
             .add(Registries.CONFIGURED_FEATURE, ConfiguredFeatures::bootstrap)
             .add(Registries.PLACED_FEATURE, PlacedFeatures::bootstrap)
             .add(Registries.BIOME, OccultismRegistries::bootstrapBiomes)
             .add(ForgeRegistries.Keys.BIOME_MODIFIERS, BiomeModifiers::bootstrap);
-
-    public static HolderLookup.Provider createLookup() {
-        var registryaccess = RegistryAccess.fromRegistryOfRegistries(BuiltInRegistries.REGISTRY);
-        var holderlookup = BUILDER.build(registryaccess);
-        return holderlookup;
-    }
 
     public static void bootstrapBiomes(BootstapContext<Biome> context) {
         //doesn't need to do anything, just gives us acccess to a biome registry with empty tag lookup in our other boopstrap contexts
