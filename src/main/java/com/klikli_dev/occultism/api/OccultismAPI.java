a221055a7be67b110ba88f57b0e5c4f56d56108d
Kli Kli <klikli@outlook.at>
2023-07-26 15:42:34 +0200

    feat: add helpers for spirit item pick up to api

--- src/main/java/com/klikli_dev/occultism/api/OccultismAPI.java
+++ src/main/java/com/klikli_dev/occultism/api/OccultismAPI.java
@@ -22,6 +22,44 @@
 
 package com.klikli_dev.occultism.api;
 
+import com.klikli_dev.occultism.common.entity.job.SpiritJob;
+import com.klikli_dev.occultism.common.entity.spirit.SpiritEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.List;
+import java.util.Optional;
+
 public class OccultismAPI {
 
+    private static final OccultismAPI instance = new OccultismAPI();
+
+    public static OccultismAPI get() {
+        return instance;
+    }
+
+    /**
+     * Gets the list of items the given entity can pick up, IF that entity is an occultism spirit with a job.
+     *
+     * @return An empty optional if it is not a spirit entity, or the spirit entity has no job, otherwise the list of items it can pick up.
+     */
+    Optional<List<Ingredient>> getItemsToPickUp(Entity entity) {
+        if (entity instanceof SpiritEntity spiritEntity) {
+            return spiritEntity.getJob().map(SpiritJob::getItemsToPickUp);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Checks if the given entity can pick up the given item entity, IF that entity is an occultism spirit with a job.
+     *
+     * @return An empty optional if it is not a spirit entity, or the spirit entity has no job, otherwise a boolean representing whether it can pick up the item.
+     */
+    public Optional<Boolean> canPickupItem(Entity entity, ItemEntity itemEntity) {
+        if (entity instanceof SpiritEntity spiritEntity) {
+            return spiritEntity.getJob().map(job -> job.canPickupItem(itemEntity));
+        }
+        return Optional.empty();
+    }
 }
