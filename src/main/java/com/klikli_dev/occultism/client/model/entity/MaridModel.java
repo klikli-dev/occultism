966835d08126dbbd61164492b0b5dfca9b5c8eae
Kli Kli <klikli@outlook.at>
2024-01-22 07:01:58 +0100

    feat: add new marid model - thank @FeellianSinger

--- src/main/java/com/klikli_dev/occultism/client/model/entity/MaridModel.java
+++ src/main/java/com/klikli_dev/occultism/client/model/entity/MaridModel.java
@@ -22,6 +22,8 @@
 
 package com.klikli_dev.occultism.client.model.entity;
 
+import com.klikli_dev.occultism.Occultism;
+import com.klikli_dev.occultism.common.entity.spirit.AfritEntity;
 import com.klikli_dev.occultism.common.entity.spirit.MaridEntity;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
@@ -30,36 +32,16 @@ import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.model.geom.builders.MeshDefinition;
 import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.model.DefaultedEntityGeoModel;
+import software.bernie.geckolib.model.DefaultedGeoModel;
 
 
-public class MaridModel extends HumanoidModel<MaridEntity> {
+public class MaridModel extends DefaultedEntityGeoModel<MaridEntity> {
 
-    public ModelPart nose;
-    public ModelPart earLeft;
-    public ModelPart earRight;
-
-    public MaridModel(ModelPart part) {
-        super(part);
-
-        this.nose = this.head.getChild("nose");
-        this.earLeft = this.head.getChild("left_ear");
-        this.earRight = this.head.getChild("right_ear");
+    public MaridModel() {
+        super(new ResourceLocation(Occultism.MODID, "marid"), true);
     }
-
-    public static LayerDefinition createBodyLayer() {
-        MeshDefinition mesh = new MeshDefinition();
-        PartDefinition parts = mesh.getRoot();
-        PartDefinition head = parts.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -10.0F, -4.0F, 8, 10, 8, false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0, 0, 0));
-        PartDefinition hat = parts.addOrReplaceChild("hat", CubeListBuilder.create().texOffs(1, 45).addBox(-4.0F, -8.0F, -4.0F, 8, 10, 8, false), PartPose.offsetAndRotation(0.0F, -3.0F, 0.0F, 0, 0, 0));
-        PartDefinition body = parts.addOrReplaceChild("body", CubeListBuilder.create().texOffs(16, 20).addBox(-4.0F, 0.0F, -3.0F, 8, 12, 6, false), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0, 0, 0));
-        PartDefinition rightArm = parts.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(44, 22).addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, false), PartPose.offsetAndRotation(-5.0F, 3.0F, -1.0F, -0.7499679795819634F, 0.0F, 0.0F));
-        PartDefinition leftArm = parts.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(44, 22).addBox(-1.0F, -2.0F, -2.0F, 4, 11, 4, false), PartPose.offsetAndRotation(5.0F, 3.0F, -1.0F, -0.7499679795819634F, 0.0F, 0.0F));
-        PartDefinition rightLeg = parts.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 22).addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, false), PartPose.offsetAndRotation(-2.0F, 12.0F, 0.0F, 0, 0, 0));
-        PartDefinition leftLeg = parts.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 22).addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, true), PartPose.offsetAndRotation(2.0F, 12.0F, 0.0F, 0, 0, 0));
-        PartDefinition nose = head.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(24, 0).addBox(-1.0F, 0.0F, 0.0F, 2, 4, 2, false), PartPose.offsetAndRotation(0.0F, -5.0F, -4.4F, -0.4553564018453205F, 0.0F, 0.0F));
-        PartDefinition earLeft = head.addOrReplaceChild("left_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1, false), PartPose.offsetAndRotation(4.0F, -11.3F, 3.5F, -0.5009094953223726F, 0.0F, 0.0F));
-        PartDefinition earRight = head.addOrReplaceChild("right_ear", CubeListBuilder.create().texOffs(0, 0).addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1, false), PartPose.offsetAndRotation(-4.0F, -11.3F, 3.5F, -0.5009094953223726F, 0.0F, 0.0F));
-        return LayerDefinition.create(mesh, 64, 64);
-    }
-
 }
+
