b181f5d9b5b7fe38bba7261780d46eb485c7836d
Kli Kli <klikli@outlook.at>
2024-04-11 14:13:51 +0200

    feat: rotatable bowls (#1111)
    * feat: allow sacrificial bowls to be rotated
    
    * feat: make item bob in facing direction
    
    * fix: improve item bobbing location
    
    * fix: final fix to item bobbing location

--- src/main/java/com/klikli_dev/occultism/client/render/blockentity/SacrificialBowlRenderer.java
+++ src/main/java/com/klikli_dev/occultism/client/render/blockentity/SacrificialBowlRenderer.java
@@ -32,9 +32,13 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3f;
 
 public class SacrificialBowlRenderer implements BlockEntityRenderer<SacrificialBowlBlockEntity> {
 
@@ -57,12 +61,29 @@ public class SacrificialBowlRenderer implements BlockEntityRenderer<SacrificialB
         blockEntity.itemStackHandler.ifPresent(handler -> {
             ItemStack stack = handler.getStackInSlot(0);
             long time = blockEntity.getLevel().getGameTime();
+
+            var facing = blockEntity.getBlockState().hasProperty(BlockStateProperties.FACING) ?
+                    blockEntity.getBlockState().getValue(BlockStateProperties.FACING) : Direction.UP;
+
+            poseStack.pushPose();
+
+            //TODO: Currently the items bob up and down, instead of away from the bowl facing and back
+
             poseStack.pushPose();
 
             //slowly bob up and down following a sine
             double offset = Math.sin((time - blockEntity.lastChangeTime + partialTicks) / 16) * 0.5f + 0.5f; // * 0.5f + 0.5f;  move sine between 0.0-1.0
             offset = offset / 4.0f; //reduce amplitude
-            poseStack.translate(0.5, 0.6 + offset, 0.5);
+
+            // Fixed offset to push the item away from the bowl
+            double fixedOffset = 0.2;
+
+            // Adjust the translation based on the facing direction
+            double xOffset = facing.getAxis() == Direction.Axis.X ? (facing.getAxisDirection() == Direction.AxisDirection.POSITIVE ? offset + fixedOffset : -offset - fixedOffset) : 0.0;
+            double yOffset = facing.getAxis() == Direction.Axis.Y ? (facing.getAxisDirection() == Direction.AxisDirection.POSITIVE ? offset + fixedOffset : -offset - fixedOffset) : 0.0;
+            double zOffset = facing.getAxis() == Direction.Axis.Z ? (facing.getAxisDirection() == Direction.AxisDirection.POSITIVE ? offset + fixedOffset : -offset - fixedOffset) : 0.0;
+
+            poseStack.translate(0.5 + xOffset, 0.5 + yOffset, 0.5 + zOffset);
 
             //use system time to become independent of game time
             long systemTime = System.currentTimeMillis();
@@ -78,6 +99,10 @@ public class SacrificialBowlRenderer implements BlockEntityRenderer<SacrificialB
             BakedModel model = itemRenderer.getModel(stack, blockEntity.getLevel(), null, 0);
             itemRenderer.render(stack, ItemDisplayContext.FIXED, true, poseStack, buffer,
                     combinedLight, combinedOverlay, model);
+
+            poseStack.popPose();
+
+            poseStack.mulPose(facing.getRotation());
 
             poseStack.popPose();
         });
--- src/main/java/com/klikli_dev/occultism/common/block/SacrificialBowlBlock.java
+++ src/main/java/com/klikli_dev/occultism/common/block/SacrificialBowlBlock.java
@@ -26,18 +26,22 @@ import com.klikli_dev.occultism.common.blockentity.SacrificialBowlBlockEntity;
 import com.klikli_dev.occultism.registry.OccultismTiles;
 import com.klikli_dev.occultism.util.StorageUtil;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -47,12 +51,16 @@ import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nullable;
 
-public class SacrificialBowlBlock extends Block implements EntityBlock {
+public class SacrificialBowlBlock extends DirectionalBlock implements EntityBlock {
 
-    private static final VoxelShape SHAPE = Block.box(4, 0, 4, 12, 2.3, 12);
+    private static final DirectionalBlockShape SHAPE = new DirectionalBlockShape(12, 12, 2.3f);
+
+    //TODO Rotate renderer
 
     public SacrificialBowlBlock(Properties properties) {
         super(properties);
+
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.UP));
     }
 
     @Override
@@ -108,7 +116,21 @@ public class SacrificialBowlBlock extends Block implements EntityBlock {
     @Override
     @SuppressWarnings("deprecation")
     public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+        return SHAPE.getShape(state.getValue(FACING));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        Direction direction = pContext.getClickedFace();
+        BlockState blockstate = pContext.getLevel().getBlockState(pContext.getClickedPos().relative(direction.getOpposite()));
+        return blockstate.is(this) && blockstate.getValue(FACING) == direction
+                ? this.defaultBlockState().setValue(FACING, direction.getOpposite())
+                : this.defaultBlockState().setValue(FACING, direction);
     }
 
     @Nullable
