f7c46b1a090fe748d96bd043ae628c7fa19a50e4
Eqis-Edu <117950663+Eqis-Edu@users.noreply.github.com>
2025-01-16 21:56:21 -0300

    missing import

--- src/main/java/com/klikli_dev/occultism/common/entity/familiar/IesniumGolemEntity.java
+++ src/main/java/com/klikli_dev/occultism/common/entity/familiar/IesniumGolemEntity.java
@@ -26,6 +26,7 @@ import com.klikli_dev.occultism.registry.OccultismItems;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
