a6690debe0b2d6b5607c7f1d4dff04aed615c12a
Katrina Knight <kat.knight@newgaea.net>
2025-02-28 17:41:50 +0000

    feat: add Jade/Waila plugin that hides IOtherworldBlocks if covered, fixes #1310

---
+++ src/main/java/com/klikli_dev/occultism/integration/waila/OccultismPlugin.java
@@ -0,0 +1,41 @@
+package com.klikli_dev.occultism.integration.waila;
+
+import com.klikli_dev.occultism.Occultism;
+import com.klikli_dev.occultism.api.common.data.OtherworldBlockTier;
+import com.klikli_dev.occultism.common.block.otherworld.IOtherworldBlock;
+import com.klikli_dev.occultism.registry.OccultismBlocks;
+import com.klikli_dev.occultism.util.CuriosUtil;
+import net.minecraft.world.level.block.Blocks;
+import snownee.jade.api.*;
+
+@WailaPlugin
+public class OccultismPlugin implements IWailaPlugin {
+    @Override
+    public void register(IWailaCommonRegistration registration) {
+        //TODO register data providers
+    }
+
+    @Override
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.addRayTraceCallback((hitResult, accessor, originalAccessor) -> {
+            if(accessor!=null) {
+                boolean hasGoggles = CuriosUtil.hasGoggles(accessor.getPlayer());
+                if (accessor instanceof BlockAccessor blockAccessor) {
+                    if (blockAccessor.getBlock() instanceof IOtherworldBlock otherworldBlock) {
+                        if (otherworldBlock.getTier() == OtherworldBlockTier.ONE || hasGoggles) {
+                            if (blockAccessor.getBlockState().getValue(IOtherworldBlock.UNCOVERED)) {
+                                return registration.blockAccessor().from(blockAccessor).blockState(otherworldBlock.getUncoveredBlock().defaultBlockState()).build();
+                            } else {
+                                return registration.blockAccessor().from(blockAccessor).blockState(otherworldBlock.getCoveredBlock().defaultBlockState()).build();
+                            }
+                        } else {
+                            return registration.blockAccessor().from(blockAccessor).blockState(otherworldBlock.getCoveredBlock().defaultBlockState()).build();
+                        }
+                    }
+                }
+            }
+            return accessor;
+        });
+    }
+
+}
