b1942d5fd195c6ce71e90a0c1ce4eaaf4ba96bda
Kli Kli <klikli@outlook.at>
2026-04-25 13:09:09 +0200

    fix: use geckolib entity yaw tickets for spirit heads

--- src/main/java/com/klikli_dev/occultism/client/render/entity/OccultismGeoLivingEntityRenderer.java
+++ src/main/java/com/klikli_dev/occultism/client/render/entity/OccultismGeoLivingEntityRenderer.java
@@ -6,13 +6,13 @@
 package com.klikli_dev.occultism.client.render.entity;
 
 import com.geckolib.animatable.GeoAnimatable;
+import com.geckolib.constant.DefaultAnimations;
 import com.geckolib.renderer.base.BoneSnapshots;
 import com.geckolib.model.GeoModel;
 import com.geckolib.renderer.GeoEntityRenderer;
 import com.geckolib.renderer.base.RenderPassInfo;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import org.jspecify.annotations.Nullable;
 
@@ -30,10 +30,7 @@ public abstract class OccultismGeoLivingEntityRenderer<T extends LivingEntity & 
     public void adjustModelBonesForRender(RenderPassInfo<OccultismGeoLivingEntityRenderState> renderPassInfo, BoneSnapshots snapshots) {
         super.adjustModelBonesForRender(renderPassInfo, snapshots);
 
-        float headYaw = renderPassInfo.renderState().yRot * Mth.DEG_TO_RAD;
-        float headPitch = -renderPassInfo.renderState().xRot * Mth.DEG_TO_RAD;
-
-        snapshots.ifPresent("head", bone -> bone.setRotY(headYaw).setRotX(headPitch));
-        snapshots.ifPresent("Head", bone -> bone.setRotY(headYaw).setRotX(headPitch));
+        DefaultAnimations.hardcodedHeadRotation(renderPassInfo, snapshots, "head");
+        DefaultAnimations.hardcodedHeadRotation(renderPassInfo, snapshots, "Head");
     }
 }
