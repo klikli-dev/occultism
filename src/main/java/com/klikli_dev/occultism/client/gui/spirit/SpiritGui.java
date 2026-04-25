b0096631b01a7cfc6fa5e44217a5a65b0690c779
Kli Kli <klikli@outlook.at>
2026-04-25 14:55:40 +0200

    fix: raise worker gui spirit previews

--- src/main/java/com/klikli_dev/occultism/client/gui/spirit/SpiritGui.java
+++ src/main/java/com/klikli_dev/occultism/client/gui/spirit/SpiritGui.java
@@ -54,7 +54,6 @@ public class SpiritGui<T extends SpiritContainer> extends AbstractContainerScree
     private static final int ENTITY_RENDER_HEIGHT = 70;
     private static final int ENTITY_BASE_SCALE = 30;
     private static final int ENTITY_FIT_PADDING = 14;
-    private static final int ENTITY_RENDER_Y_OFFSET = ENTITY_RENDER_HEIGHT / 4;
 
     protected static final Identifier TEXTURE = Identifier.fromNamespaceAndPath(Occultism.MODID,
             "textures/gui/inventory_spirit.png");
@@ -201,7 +200,7 @@ public class SpiritGui<T extends SpiritContainer> extends AbstractContainerScree
 
         guiGraphics.pose().pushMatrix();
         int entityX = this.leftPos + 35;
-        int entityY = this.topPos + 65 + ENTITY_RENDER_Y_OFFSET;
+        int entityY = this.topPos + 65;
         int scale = getEntityScale(this.spirit.getEntity());
         drawEntityToGui(guiGraphics, entityX, entityY, scale, getEntityMouseX(entityX), getEntityMouseY(entityY),
                 this.spirit.getEntity());
