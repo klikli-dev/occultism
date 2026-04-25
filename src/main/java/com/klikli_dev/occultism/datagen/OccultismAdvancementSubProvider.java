eb8f6f15974bc833fa2431bcf66127ff785aefeb
Kli Kli <klikli@outlook.at>
2026-04-25 12:55:53 +0200

    fix: advancement bg

--- src/main/java/com/klikli_dev/occultism/datagen/OccultismAdvancementSubProvider.java
+++ src/main/java/com/klikli_dev/occultism/datagen/OccultismAdvancementSubProvider.java
@@ -94,16 +94,16 @@ public class OccultismAdvancementSubProvider implements AdvancementSubProvider {
 
     private void start() {
         var root = this.add(Builder.advancement()
-                .display(new DisplayInfo(
-                        new ItemStackTemplate(OccultismItems.JEI_DUMMY_NONE.get().builtInRegistryHolder()),
-                        title("root"),
-                        descr("root"),
-                        Optional.of(new ClientAsset.ResourceTexture(Identifier.fromNamespaceAndPath("minecraft", "gui/advancements/backgrounds/stone"))),
-                        AdvancementType.TASK,
-                        true,
-                        true,
-                        true)
-                )
+//                .display(
+//                        new ItemStackTemplate(OccultismItems.JEI_DUMMY_NONE.get().builtInRegistryHolder()),
+//                        title("root"),
+//                        descr("root"),
+//                        Identifier.fromNamespaceAndPath(Occultism.MODID,"block/otherstone"),
+//                        AdvancementType.TASK,
+//                        true,
+//                        true,
+//                        true
+//                )
                 .addCriterion("occultism_present", PlayerTrigger.TriggerInstance.tick())
                 .build(Identifier.fromNamespaceAndPath(Occultism.MODID, "occultism/root")));
 
@@ -111,8 +111,11 @@ public class OccultismAdvancementSubProvider implements AdvancementSubProvider {
                 .display(OccultismItems.PENTACLE_POSSESS.get(),
                         title("familiars"),
                         descr("familiars"),
-                        Identifier.fromNamespaceAndPath(Occultism.MODID,"textures/block/otherplanks.png"), AdvancementType.TASK, true,
-                        true, false)
+                        Identifier.fromNamespaceAndPath(Occultism.MODID,"block/otherplanks"),
+                        AdvancementType.TASK,
+                        true,
+                        true,
+                        false)
                 .addCriterion("summon_familiar",
                         TriggerInstance.ritualFactory(OccultismRituals.FAMILIAR.getId()))
                 .build(Identifier.fromNamespaceAndPath(Occultism.MODID, "occultism/familiar/root")));
