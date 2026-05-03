2d5203685dd8012c1187fe3aff0aa7adf7133be0
Kli Kli <klikli@outlook.at>
2026-05-03 12:00:00 +0200

    fix: refine foliot held item placement

--- src/main/java/com/klikli_dev/occultism/client/render/entity/FoliotRenderer.java
+++ src/main/java/com/klikli_dev/occultism/client/render/entity/FoliotRenderer.java
@@ -86,8 +86,8 @@ public class FoliotRenderer extends OccultismGeoLivingEntityRenderer<FoliotEntit
             @Override
             protected void submitItemStackRender(PoseStack poseStack, GeoBone bone, ItemStackRenderState stackState, ItemDisplayContext displayContext, OccultismGeoLivingEntityRenderState renderState, SubmitNodeCollector renderTasks, int packedLight) {
                 poseStack.pushPose();
-                poseStack.translate(0, -0.4, 0);
-                poseStack.scale(0.33333334F, 0.33333334F, 0.33333334F);
+                poseStack.translate(-0.12, -0.3, 0);
+                poseStack.scale(0.4F, 0.4F, 0.4F);
                 if (Objects.equals(renderState.jobID, OccultismSpiritJobs.CLEANER.getId().toString())) {
                     poseStack.translate(-0.3, -0.35, 0.35);
                     poseStack.scale(0.5F, 0.5F, 0.5F);
