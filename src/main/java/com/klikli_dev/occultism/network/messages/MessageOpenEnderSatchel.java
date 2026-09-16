a00d1d1aaba5f2c512b503920a4ef5c4696f28cc
Klaas058 <klaas.vanderwerk@gmail.com>
2026-09-17 01:58:31 +0200

    fix: find satchels and the storage remote in the first inventory slot

--- src/main/java/com/klikli_dev/occultism/network/messages/MessageOpenEnderSatchel.java
+++ src/main/java/com/klikli_dev/occultism/network/messages/MessageOpenEnderSatchel.java
@@ -70,7 +70,7 @@ public class MessageOpenEnderSatchel implements IMessage {
         //if not found, try to get from player inventory
         if (!(backpackStack.getItem() instanceof EnderSatchelItem)) {
             selectedSlot = CuriosUtil.getFirstEnderSatchelSlot(player);
-            backpackStack = selectedSlot > 0 ? player.getInventory().getItem(selectedSlot) : ItemStack.EMPTY;
+            backpackStack = selectedSlot >= 0 ? player.getInventory().getItem(selectedSlot) : ItemStack.EMPTY;
         }
         //now, if we have a satchel, proceed
         if (backpackStack.getItem() instanceof EnderSatchelItem enderSatchelItem) {
